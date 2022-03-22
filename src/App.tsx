a63f13607d35d4d2986cd416b031231b0edc620f
Adam oberembt <adamoberembt@adams-mbp-3.myfiosgateway.com>
2022-03-22 15:40:31 -0400

    added counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
+    const [value, setValue] = useState<number>(0);
     return (
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <span>WELCOME! To Group 29 Web Page</span>
+            <p>
+                <span>
+                    <Button onClick={() => setValue(1 + value)}>Add One</Button>
+                    to {value}.
+                </span>
+            </p>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
